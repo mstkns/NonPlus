3ddeb01998a22fad0a3e23967003f19e944ce492
mstkns <mar98kus@gmail.com>
2025-05-29 02:38:22 +0200

    Implemented lexing of integer literals

--- lexer.c
+++ lexer.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <ctype.h>
 #include "lexer.h"
 
 Lexer new_lexer (const char *content) {
@@ -8,4 +9,31 @@ Lexer new_lexer (const char *content) {
       .cursor         = 0
    };
    return l;
+}
+
+Token next_token (Lexer *l) {
+
+   // Skip whitespace
+   while (l->cursor < l->content_length && isspace (l->content [l->cursor])) {
+      l->cursor++;
+   }
+
+   Token t = {
+      .name  = UNKNOWN,
+      .value = &l->content [l->cursor],
+      .length = 0
+   };
+
+   if (isdigit (l->content [l->cursor])) {
+      t.name = LITERAL;
+      l->cursor++;
+      t.length++;
+      while (isdigit (l->content [l->cursor])) {
+         l->cursor++;
+         t.length++;
+      }
+   }
+   
+   return t;
+
 }
